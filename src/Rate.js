87b5b807e89758f3ab41487b21f5c65339140b56
Akash <akashbhadoriya00@gmail.com>
2022-12-06 21:59:19 +0530

    new version

--- src/Rate.js
+++ src/Rate.js
@@ -5,13 +5,11 @@ const Rate = ({name,unit,value,type}) => {
   return (
     <div className='rate-container'>
         <div className='rate-row'>
-            <div className='rate'>
-                <h1>{name}</h1>
-            </div>
             <div className='rate-data'>
-                <p className='rate-unit'>{unit}</p>
-                <p className='rate-value'>{value}</p>
-                <p className='rate-type'>{type}</p>
+              <h1>{name}</h1>
+              <p className='rate-unit'>{unit}</p>
+              <p className='rate-value'>{value}</p>
+              <p className='rate-type'>{type}</p>
             </div>
         </div>
     </div>
